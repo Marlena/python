80b6bde746c04520ed98472541da66532769b419
mcompton <mcompton@bluemoon.sydney.atlassian.com>
2011-02-14 18:33:35 +1100

    factorial script

---
+++ factorial.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+def factorial(x):
+    if (x > 1):
+        return x * factorial(x - 1)
+    else:
+        return x
+
+def printFactorial(x):
+    print str(factorial(x) )
+
+
+printFactorial(5)
